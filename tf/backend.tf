8786f051e57341b4f0cc724ab6266e12d441d6d9
KimangMac14 <kimangkheang@gmail.com>
2024-08-15 12:04:48 +0700

    Ansible

--- tf/backend.tf
+++ tf/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "tf-state"
     key    = "tf"
-    region = "us-east-1"
+    region = "ap-southeast-1"
   }
 }
--- tf/main.tf
+++ tf/main.tf
@@ -10,7 +10,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-southeast-1"
 }
 
 resource "aws_security_group" "sg_1" {
@@ -33,40 +33,46 @@ resource "aws_security_group" "sg_1" {
 }
 
 
-resource "aws_key_pair" "kimang_key" {
-  key_name   = "kimang-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDhC/kNEer6SHxRh1BWChkxx0Dr4crG1kkqCqPiwFThtHbkuq7pHksB4/+m+oOfBhEndkYGLv2ezLQYClIqm+1NnR2NWv2OrsjwMzU1xmmczS0obzFRxA2uYcninAM13qTqT89wYz7A6rxi4ZeCHgbjya6iz+FIvZcNMZY8CAQuq7e8X2QM4DlllXJ4rbtWhZok7gVrv43lLhOfHQbrjV2fyTla0Eq8PCjjuGG78+8XvSlASaBxkUTR9bHjVXVi0oGgbIeN9Cj8tzzL5kkK6ubCvhiwkd2p2heFgsOPeIQhFrKEO1aj4B0eY0Nwaby+91dlYv6Y5DqPBUr7Z5KdwyOV kimang@KIMs-MacBook-Pro.local"
+resource "aws_key_pair" "ec2_key" {
+  key_name   = "ec2-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDHaM/DhbzbM0sG5DBP5ldfPFzguYJdtg/tl9/Uw3FKXLYPHauoiYqCztKtyuek1nGExSAeLPqUdOPafBjFgDk+Ziz8fmj4/rUkSmNeIdddsZGpxYhb8F273ftCrIhlvk+KX+AsvffJUiGvZ/dALC8EG1E2YXUSlB2alM1VHiCbNHV5AUWVteIWho99chO6WAljFH7R6kq23t/eP5hPOmd0Gtgw0H8PCMab58GbItviJGcigTct0lBTx1DSCEGpUobvEjnSBJ8ZnDsZT9CPRHcWe8u8RrOUzeI7hP+YA7kR28vIH6btsy1MtPufFimTNTnGoPc2Emy1NWPpkA7B1+CR kimang@KIMs-MacBook-Pro.local"
 }
 
 resource "aws_instance" "server_1" {
-  ami  = "ami-ff0fea8310f3"
-  instance_type = "t3.micro"
-  count = 2
-  key_name = aws_key_pair.kimang_key.key_name
+  ami             = "ami-ff0fea8310f3"
+  instance_type   = "t3.micro"
+  count           = 3
+  key_name        = aws_key_pair.ec2_key.key_name
   security_groups = [aws_security_group.sg_1.name]
+  # user_data                   = <<-EOF
+  #             #!/bin/bash
+  #             apt update -y
+  #             apt install git -y
+  #             apt install curl -y
+  #             apt install ping -y
+
+  #             # Install NVM
+  #             curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+  #             . ~/.nvm/nvm.sh
+
+  #             # Install Node.js 18
+  #             nvm install 18
+
+  #             # Install PM2
+  #             npm install pm2 -g
+
+  #             # Clone Node.js repository
+  #             git clone https://github.com/KimangKhenng/devops-ex /root/devops-ex
+
+  #             # Navigate to the repository and start the app with PM2
+  #             cd /root/devops-ex
+  #             npm install
+  #             pm2 start app.js --name node-app -- -p 8000
+  #           EOF
+  user_data_replace_on_change = true
   user_data = <<-EOF
               #!/bin/bash
-              apt update
-              apt install git -y
-              apt install curl -y
-
-              # Install NVM
-              curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-              . ~/.nvm/nvm.sh
-
-              # Install Node.js 18
-              nvm install 18
-
-              # Install PM2
-              npm install pm2 -g
-
-              # Clone Node.js repository
-              git clone https://github.com/KimangKhenng/devops-ex /root/devops-ex
-
-              # Navigate to the repository and start the app with PM2
-              cd /root/devops-ex
-              npm install
-              pm2 start app.js --name node-app -- -p 8000
+              apt update -y
+              apt install python3 -y
             EOF
-  user_data_replace_on_change = true
 }
